bcf1c90302010ae53f54e15a3d333afa0500cc3e
Nyambi Nyambi <nyambisapothecary@gmail.com>
2025-01-21 23:10:50 -0600

    Refactor chapter3.py to improve function organization and add docstrings. Created statements_and_expressions function.

--- chapter_3_syntax_crash_course/chapter3.py
+++ chapter_3_syntax_crash_course/chapter3.py
@@ -4,21 +4,27 @@ playground for exploring and practicing
 different Python syntax features.
 """
 
-
 def print_hello_world():
     """Prints the classic 'Hello, world!' message."""
     print("Hello, world!")
-
-
-# print() function writes text to the console.
-print_hello_world()
-
 
 def get_input():
     """Gets input from the console"""
     name = input("What is your name? ")
     print("Hello, " + name)
 
+def statements_and_expressions():
+    """
+    - Each line of code in Python that ends with a line break is a statement.
+    - An expression is a section of code that evaluates to a single value.
+    - 1. Assign the expression "Hello, world!" to message ... 
+    - 2. then pass the expression message to print()
+    """
+    message = "Hello, world"
+    print(message)
 
-# input() function gets input from the console.
-get_input()
+if __name__ == "__main__":
+    """ You can call any function here to run it independently"""
+    #print_hello_world()
+    #get_input()
+    statements_and_expressions()
