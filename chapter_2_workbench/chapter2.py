818088c355af4673f831d159810aacbba6d4955f
Nyambi Nyambi <nyambisapothecary@gmail.com>
2025-01-21 21:36:21 -0600

    Wrapped up Chapter 2 reading with print statement to test executable in terminal.

--- chapter_2_workbench/chapter2.py
+++ chapter_2_workbench/chapter2.py
@@ -1,2 +1,12 @@
 #!/user/bin/env python3
+
+'''
+ Used this to test reformatting with 'Black'
+original code (spacing issue):
+x="yp" 
+'''
 x = "yp"
+
+# Checking to see if the hashbang works (made this an executable)
+
+print("Hello, world")
