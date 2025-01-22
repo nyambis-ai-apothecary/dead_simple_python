858667756e76f040102b17b2e5e0b3fb27585aa4
Nyambi Nyambi <nyambisapothecary@gmail.com>
2025-01-22 00:27:21 -0600

    Revised docstring text to adhere better to Python standards.

--- chapter_3_syntax_crash_course/chapter3.py
+++ chapter_3_syntax_crash_course/chapter3.py
@@ -6,22 +6,23 @@ different Python syntax features.
 
 
 def print_hello_world():
-    """Prints the classic 'Hello, world!' message."""
+    """Print the classic 'Hello, world!' message."""
     print("Hello, world!")
 
 
 def get_input():
-    """Gets input from the console"""
+    """Get input from the console and greet the user."""
     name = input("What is your name? ")
     print("Hello, " + name)
 
 
 def statements_and_expressions():
     """
+    Demonstrate statements and expressions in Python.
+
     - Each line of code in Python that ends with a line break is a statement.
     - An expression is a section of code that evaluates to a single value.
-    - 1. Assign the expression "Hello, world!" to message ...
-    - 2. then pass the expression message to print()
+    - Assign the expression "Hello, world!" to message and then pass the expression message to print().
     """
     message = "Hello, world"
     print(message)
@@ -29,9 +30,12 @@ def statements_and_expressions():
 
 def importance_whitespace():
     """
-    A compound statement  is made up of two or more clauses, each of which:
-        - starts with a header
-        - and a block of code called a suite.
+    Demonstrate the importance of whitespace in Python.
+
+    A compound statement is made up of two or more clauses, each of which:
+    - starts with a header
+    - and a block of code called a suite.
+
     The program below prints different messages, depending on whether a name is provided.
     """
     name = "Jason"
