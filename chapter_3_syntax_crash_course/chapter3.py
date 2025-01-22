d81179367f2bdce220351842ed24ed1d4060d6eb
Nyambi Nyambi <nyambisapothecary@gmail.com>
2025-01-21 22:22:35 -0600

    In the beginning, there was the print() function.

---
+++ chapter_3_syntax_crash_course/chapter3.py
@@ -0,0 +1,14 @@
+"""
+This module serves as a reference and
+playground for exploring and practicing
+different Python syntax features.
+"""
+
+
+def print_hello_world():
+    """Prints the classic 'Hello, world!' message."""
+    print("Hello, world!")
+
+
+# Call the function to print the message
+print_hello_world()
